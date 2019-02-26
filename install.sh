717c2058403cc2e55d42ec41937cb37ff0cac1d1
Woland <me@woland.xyz>
2019-02-26 11:47:58 +0100

    Add zsh

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+
 git submodule init
 git submodule update --remote --recursive
 
 stow polybar
 stow dunst
 
-mkdir -p "~/.cache/dein/"
-git clone https://github.com/Shougo/dein.vim "~/.cache/dein/repos/github.com/Shougo/dein.vim"
+# Neovim with dein
+mkdir -p "${XDG_CACHE_HOME}/dein/"
+git clone https://github.com/Shougo/dein.vim "${XDG_CACHE_HOME}/dein/repos/github.com/Shougo/dein.vim"
 stow neovim
+
+# Zsh & oh-my-zsh & bullet-train theme
+git clone https://github.com/robbyrussell/oh-my-zsh "${XDG_CACHE_HOME}/oh-my-zsh/"
+install zsh/bullet-train.zsh-theme "${XDG_CACHE_HOME}/oh-my-zsh/themes/"
+stow zsh --ignore="*.zsh-theme"
