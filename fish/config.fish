c13bfb7c5750ac6b702cf287c92bb42de8f311e0
Paweł Gronowski <me@woland.xyz>
2023-08-05 13:09:31 +0200

    fish: set nvim as EDITOR
    Signed-off-by: Paweł Gronowski <me@woland.xyz>

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,16 @@
 set fish_greeting
 
-set --local theme Light
+set --local theme Dark
 
 if test -f /usr/bin/defaults
     set --local theme (defaults read -g AppleInterfaceStyle 2>/dev/null)
 end
 
-if test "$theme" = "Dark"
-    /usr/local/bin/theme.sh hemisu-dark
-else
-    /usr/local/bin/theme.sh google-light
+if status is-interactive
+    export EDITOR=nvim
+    if test "$theme" = "Dark"
+        theme.sh hemisu-dark
+    else
+        theme.sh google-light
+    end
 end
-
