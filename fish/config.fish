09c4f2accba1f7e8f04c1e9e8df7028e1eb62706
Paweł Gronowski <me@woland.xyz>
2023-08-05 13:09:31 +0200

    fish: set nvim as EDITOR
    Signed-off-by: Paweł Gronowski <me@woland.xyz>

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     set fish_greeting
-    export EDITOR=vim
+    export EDITOR=nvim
     ~/.local/bin/theme.sh japanesque
     # Commands to run in interactive sessions can go here
 end
