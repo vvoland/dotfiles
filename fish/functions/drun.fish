2b96cfae1b7439d061f4ad29497e2ea84d7e9afd
Paweł Gronowski <me@woland.xyz>
2024-05-21 00:27:47 +0200

    fish functions
    Signed-off-by: Paweł Gronowski <me@woland.xyz>

--- fish/functions/drun.fish
+++ fish/functions/drun.fish
@@ -1,4 +1,4 @@
 function drun --wraps='docker run --cap-drop ALL --security-opt no-new-privileges -u 1000:1000 -v (pwd):(pwd) -w (pwd)' --wraps='docker run --rm --cap-drop ALL --security-opt no-new-privileges -u 1000:1000 -v (pwd):(pwd) -w (pwd)' --description 'alias drun=docker run --rm --cap-drop ALL --security-opt no-new-privileges -u 1000:1000 -v (pwd):(pwd) -w (pwd)'
-  docker run --rm --cap-drop ALL --security-opt no-new-privileges -u 1000:1000 -v (pwd):(pwd) -w (pwd) $argv
+  docker run --init --rm --cap-drop ALL --security-opt no-new-privileges -u 1000:1000 -v (pwd):(pwd) -w (pwd) $argv
         
 end
