78b7af16b5a8c811378497cc7da54e8e1334040d
Paweł Gronowski <pawel.gronowski@docker.com>
2025-05-13 13:29:36 +0200

    macbook
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- fish/functions/ffixup.fish
+++ fish/functions/ffixup.fish
@@ -1,4 +1,4 @@
-function ffixup --argument log-cmd
+function ffixup
 set files (git diff --name-only --cached | head -n1 | xargs)
 set commit (git log --pretty=oneline $files | fzf --tiebreak=index | awk "{printf \$1}")
 git commit --fixup $commit
