58188f63651bfd1cfc5481598e7fdce922e36749
Paweł Gronowski <pawel.gronowski@docker.com>
2025-08-13 14:03:29 +0200

    neovim: new config
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

---
+++ neovim/lua/plugins.lua
@@ -0,0 +1,134 @@
+require("lazy").setup({
+  "nvim-lualine/lualine.nvim",
+  "nvim-telescope/telescope.nvim",
+  "tpope/vim-sleuth",
+  "nvim-treesitter/nvim-treesitter",
+  "neovim/nvim-lspconfig",
+  { "hrsh7th/nvim-cmp", dependencies = { "hrsh7th/cmp-nvim-lsp" } },
+  {
+    'jesseleite/nvim-noirbuddy',
+    dependencies = {
+      { 'tjdevries/colorbuddy.nvim' }
+    },
+  },
+  {"hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+  },
+})
+
+require("noirbuddy").setup({
+  colors = {
+    primary = '#FFE135',
+    --primary = '#008080',
+  }
+})
+
+local map = vim.keymap.set
+
+-- Telescope
+local telescope = require("telescope.builtin")
+map("n", "<C-p>", telescope.find_files, { desc = "Find Files" })
+map("n", "<leader>fg", telescope.live_grep, { desc = "Live Grep" })
+map("n", "<C-b>", telescope.buffers, { desc = "Find Buffers" })
+
+-- treesitter
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "lua", "python", "go", "bash", "markdown" },
+  highlight = { enable = true },
+})
+
+--vim.api.nvim_create_user_command("Reload", reload, {})
+
+-- lspconfig
+
+local on_attach = function(_, bufnr)
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+  map("n", "gd", vim.lsp.buf.definition, opts)
+  map("n", "<leader>h", vim.lsp.buf.hover, opts)
+  map("n", "gi", vim.lsp.buf.implementation, opts)
+  map("n", "gr", vim.lsp.buf.references, opts)
+  map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  map("n", "<leader><space>", vim.lsp.buf.code_action, opts)
+end
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require("lspconfig")
+
+lspconfig.gopls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      experimentalPostfixCompletions = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+})
+
+
+lspconfig.bashls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+
+-- cmp
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<CR>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.confirm({ select = true })
+        vim.defer_fn(function()
+          vim.lsp.buf.signature_help()
+        end, 100)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  }),
+})
