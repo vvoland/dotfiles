d93f783220d2b63d3128f89e6b3cf545bd751f70
Paweł Gronowski <pawel.gronowski@docker.com>
2025-08-13 14:03:29 +0200

    neovim: new config
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

---
+++ neovim/lua/options.lua
@@ -0,0 +1,14 @@
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.hidden = true
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.mouse = ""
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.termguicolors = true
+vim.g.mapleader = "\\"
+-- vim.cmd("colorscheme wal")
+
