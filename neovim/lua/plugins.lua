123c2c00525fa2cbd5e8e32f55b317b0c72952ae
Paweł Gronowski <pawel.gronowski@docker.com>
2025-08-14 11:01:54 +0200

    nvim lua
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
+      { "hrsh7th/cmp-nvim-lua", commit = "f12408bdb54c39c23e67cab726264c10db33ada8" },
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
     },
@@ -177,5 +178,6 @@ cmp.setup({
     { name = "luasnip" },
     { name = "buffer" },
     { name = "path" },
+    { name = "nvim_lua" },
   }),
 })
