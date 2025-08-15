60dab7232c9626c0747c0f67130b3c759f50a5df
Paweł Gronowski <me@woland.xyz>
2025-08-16 01:13:50 +0200

    kitty reformat
    Signed-off-by: Paweł Gronowski <me@woland.xyz>

--- kitty/host.py
+++ kitty/host.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import socket
 
-hostname=socket.gethostname()
+hostname = socket.gethostname()
 
-windrider="""
-font_size 16
+windrider = """
+font_size 14
 font_family      family="Adwaita Mono"
 
 bold_font        auto
@@ -12,7 +12,7 @@ italic_font      auto
 bold_italic_font auto
 """
 
-macbook="""
+macbook = """
 shell /opt/homebrew/bin/fish
 font_size 14
 font_family      family="Terminus (TTF)"
@@ -24,14 +24,16 @@ background_opacity 0.9
 background_blur 64
 """
 
+
 def main():
     h = hostname.lower()
     cfg = {
-            "windrider": windrider,
-            "fqpf6ww46h": macbook,
-            "macbookpro.lan": macbook,
+        "windrider": windrider,
+        "fqpf6ww46h": macbook,
+        "macbookpro.lan": macbook,
     }
     print(cfg[h])
+
 
 if __name__ == "__main__":
     main()
