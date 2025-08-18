f9539025a3ca479e27c0ed8001586dff72ba72bb
Paweł Gronowski <pawel.gronowski@docker.com>
2025-08-18 16:02:53 +0200

    fix gh pr
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- fish/functions/gh-pr.fish
+++ fish/functions/gh-pr.fish
@@ -11,8 +11,14 @@ function gh-pr --argument-names nofill
         exit
     end
 
-    set body (git log --reverse --format="### %s%n%n%b" (git merge-base HEAD upstream)...HEAD | grep -v Signed-off)
-    set title (git log --reverse --format="%s" (git merge-base HEAD upstream)...HEAD | head -n1)
+    set master (git merge-base HEAD upstream/master)
+    if test -z "$master"
+        set master (git merge-base HEAD upstream/main)
+    end
+    set merge_base "$master...HEAD"
 
-    gh pr create -w --head "vvoland:$branch" -R "$repo" --title "$title" -a "@me" --body "$body" -B "$BASE" "$argv"
+    set body (git log --reverse --format="### %s%n%n%b" "$merge_base" | grep -v Signed-off)
+    set title (git log --reverse --format="%s" "$merge_base" | head -n1)
+
+    echo "$body" | gh pr create -w --head "vvoland:$branch" -R "$repo" --title "$title" -a "@me" --body @ $argv
 end
