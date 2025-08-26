8a2ca004ce3809a281925d2d66977f32a90ebb20
Paweł Gronowski <pawel.gronowski@docker.com>
2025-08-26 11:01:26 +0200

    lua/secrets: support security cmd
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- neovim/lua/secrets.lua
+++ neovim/lua/secrets.lua
@@ -1,6 +1,15 @@
 local M = {}
 
 function M.pass_func(key_name)
+  if vim.fn.executable("security") then
+    local handle = io.popen("security find-generic-password -w -s " .. key_name)
+    local result = handle:read("*l")
+    handle:close()
+    if result then
+      return function() return result end
+    end
+  end
+
   local handle = io.popen("pass show " .. key_name)
   local result = handle:read("*l")
   handle:close()
