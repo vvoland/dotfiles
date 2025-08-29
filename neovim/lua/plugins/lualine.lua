e7ea989a48b702a8e987e31406c330074581049e
Paweł Gronowski <pawel.gronowski@docker.com>
2025-08-29 17:06:08 +0200

    lualine: show full path
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -13,8 +13,19 @@ return {
         disabled_filetypes = {},
         always_divide_middle = true,
       },
-      -- sections = noirbuddy_lualine.sections,
-      -- inactive_sections = noirbuddy_lualine.inactive_sections,
+      sections = {
+        lualine_a = {"mode"},
+        lualine_b = {"branch", "diff", "diagnostics"},
+        lualine_c = {
+          {
+            "filename",
+            path = 3, -- Show the full file path
+          }
+        },
+        lualine_x = {"lsp_status", "encoding", "fileformat", "filetype"},
+        lualine_y = {"progress"},
+        lualine_z = {"location"},
+      },
     },
-}
+  }
 }
