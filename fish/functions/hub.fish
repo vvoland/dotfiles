6553f2f4f5fa69344587e64297e5e4a1eb3c6e0c
Paweł Gronowski <pawel.gronowski@docker.com>
2025-11-27 14:08:02 +0100

    fix hub function
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- fish/functions/hub.fish
+++ fish/functions/hub.fish
@@ -3,23 +3,23 @@ function hub --argument action --argument ref
     set repo $r[1]
     set tag  $r[2]
     set digest $r[3]
-    set tag_or_digest "$tag"
-    if test -z "$tag"
-        set tag_or_digest "$digest"
+    set tag_or_digest "$digest"
+    if test -z "$digest"
+        set tag_or_digest "$tag"
     end
 
     set -l token (curl -fsSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$repo:pull" | jq --raw-output '.token')
 
     switch $action
         case blob
-            test -z "$repo" && echo "repo is empty" && exit 1
-            test -z "$digest" && echo "digest is empty" && exit 1
+            test -z "$repo" && echo "repo is empty" && return 1
+            test -z "$digest" && echo "digest is empty" && return 1
             set -l u "https://registry-1.docker.io/v2/$repo/blobs/$digest"
             curl -s -f -H "Authorization: Bearer $token" -L "$u"
         case manifest
-            test -z "$repo" && echo "repo is empty" && exit 1
-            test -z "$tag_or_digest" && echo "tag and digest is empty" && exit 1
+            test -z "$repo" && echo "repo is empty" && return 1
+            test -z "$tag_or_digest" && echo "tag and digest is empty" && return 1
             set -l u "https://registry-1.docker.io/v2/$repo/manifests/$tag_or_digest"
-            curl -s -f -H 'Accept: *' -H "Authorization: Bearer $token" -L "$u"
+            curl -v -s -f -H 'Accept: *' -H "Authorization: Bearer $token" -L "$u"
     end
 end
