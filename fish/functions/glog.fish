8215a33eb7ff307338111cbc266e55df76903ed2
Paweł Gronowski <pawel.gronowski@docker.com>
2025-12-18 12:30:31 +0100

    fish
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- fish/functions/glog.fish
+++ fish/functions/glog.fish
@@ -1,3 +1,3 @@
 function glog
-    git log --pretty=format:"%C(bold)%C(normal 1)  %s  %C(reset)%n%C(yellow)%H%C(reset) %C(green)%an% C(blue)%ad%C(reset)%n%C(reset) %n%w(0,2,2)%b%n" --date=format:"%y-%m-%d %H:%M:%S"
+    git log --pretty=format:"%C(bold)%C(yellow)%H%C(reset) %C(green)%an% C(blue)%ad%C(reset)%n%C(bold white)  %s  %C(reset)%C(cyan)%d%C(reset)%n---%n%w(0,2,2)%b%n" --date=format:"%y-%m-%d %H:%M:%S"
 end
