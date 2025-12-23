ee3f5662646316506d69a2a011f250854cb8950c
Paweł Gronowski <me@woland.xyz>
2025-12-23 14:08:30 +0100

    cagent
    Signed-off-by: Paweł Gronowski <me@woland.xyz>

--- fish/functions/cagent.fish
+++ fish/functions/cagent.fish
@@ -1,6 +1,32 @@
 function cagent
-    export ANTHROPIC_API_KEY=$(security find-generic-password -w -s Anthropic)
-    export OPENAI_API_KEY=$(security find-generic-password -w -s OpenAI)
-    #CAGENT_MODELS_GATEWAY=https://gw.docker.com/models
-    ~/c/cagent/dist/cagent $argv
+	set token ""
+	if test -x /usr/bin/security
+		set token (security find-generic-password -w -s OpenAI)
+	end
+	if test -z "$token" && command -v pass &>/dev/null
+		set token (pass show OpenAI)
+	end
+	set openai "$token"
+
+	set token ""
+	if test -x /usr/bin/security
+		set token (security find-generic-password -w -s Anthropic)
+	end
+	if test -z "$token" && command -v pass &>/dev/null
+		set token (pass show Anthropic)
+	end
+	set claude "$token"
+
+	#CAGENT_MODELS_GATEWAY=https://gw.docker.com/models
+	set bin /usr/bin/cagent
+	set bin $HOME/src/cagent/dist/cagent
+	TELEMETRY_ENABLED=false OPENAI_API_KEY=$openai ANTHROPIC_API_KEY=$claude $bin $argv
+	# set pwd (pwd)
+	# docker run --rm -it \
+	# 	-v "$pwd:$pwd" \
+	# 	-w "$pwd" \
+	# 	-u 1000:1000 \
+	# 	-e "OPENAI_API_KEY=$openai" \
+	# 	-e "ANTHROPIC_API_KEY=$claude" \
+	# 	cagent $argv
 end
