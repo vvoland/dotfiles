a5c8465cf272cae439d435fb4b98799e048aaaaa
Paweł Gronowski <pawel.gronowski@docker.com>
2026-01-09 13:28:02 +0100

    fish/gpush: Replace force with force-with-lease
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- fish/functions/gpush.fish
+++ fish/functions/gpush.fish
@@ -1,3 +1,11 @@
 function gpush
-    git push origin (git rev-parse --abbrev-ref HEAD) $argv
+    set -l args
+    for arg in $argv
+        if test "$arg" = "-f"; or test "$arg" = "--force"
+            set -a args "--force-with-lease"
+        else
+            set -a args $arg
+        end
+    end
+    git push origin (git rev-parse --abbrev-ref HEAD) $args
 end
