efd3987645eb222d6396a9aa3c008e09939a2357
Paweł Gronowski <pawel.gronowski@docker.com>
2026-04-13 12:51:33 +0200

    lsp: migrate to new nvim
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- neovim/lua/config/lsp.lua
+++ neovim/lua/config/lsp.lua
@@ -1,22 +1,30 @@
-local on_attach = function(_, bufnr)
-  local opts = { buffer = bufnr, noremap = true, silent = true }
-  local map = vim.keymap.set
-  map("n", "<leader>gd", vim.lsp.buf.definition, opts)
-  map("n", "<leader>h", vim.lsp.buf.hover, opts)
-  map("n", "<leader>gi", vim.lsp.buf.implementation, opts)
-  map("n", "<leader>gr", vim.lsp.buf.references, opts)
-  map("n", "<leader>rn", vim.lsp.buf.rename, opts)
-  map("n", "<leader><space>", vim.lsp.buf.code_action, opts)
-  map("n", "<leader>ff", function()
-    vim.lsp.buf.format()
-    vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
-  end, opts)
-end
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp-keymap", { clear = true }),
+  callback = function(ev)
+    local bufnr = ev.buf
+    local opts = { buffer = bufnr, noremap = true, silent = true }
+    local map = vim.keymap.set
 
-local lspconfig = require("lspconfig")
+    map("n", "<leader>gd", vim.lsp.buf.definition, opts)
+    map("n", "<leader>h", vim.lsp.buf.hover, opts)
+    map("n", "<leader>gi", vim.lsp.buf.implementation, opts)
+    map("n", "<leader>gr", vim.lsp.buf.references, opts)
+    map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    map("n", "<leader><space>", vim.lsp.buf.code_action, opts)
 
-lspconfig.gopls.setup({
-  on_attach = on_attach,
+    map("n", "<leader>ff", function()
+      vim.lsp.buf.format()
+      vim.lsp.buf.code_action({
+        context = { only = { "source.organizeImports" } },
+        apply = true,
+      })
+    end, opts)
+  end,
+})
+
+local lspcontainers = require("lspcontainers")
+
+vim.lsp.config("gopls", {
   settings = {
     gopls = {
       experimentalPostfixCompletions = true,
@@ -29,33 +37,24 @@ lspconfig.gopls.setup({
   },
 })
 
-local lspcontainers = require("lspcontainers")
-
-
-lspconfig.jsonls.setup({
-  on_attach = on_attach,
-})
-lspconfig.html.setup({
-  on_attach = on_attach,
-})
-lspconfig.bashls.setup({
-  on_attach = on_attach,
-})
-lspconfig.pylsp.setup({
+vim.lsp.config("pylsp", {
   cmd = lspcontainers.command("pylsp"),
-  on_attach = on_attach,
-})
-lspconfig.terraformls.setup({
-  on_attach = on_attach,
 })
 
-local util = require("lspconfig/util")
-
-lspconfig.yamlls.setup {
+vim.lsp.config("yamlls", {
   before_init = function(params)
     params.processId = vim.NIL
   end,
-  cmd = lspcontainers.command("yamlls"),
-  image = "quay.io/redhat-developer/yaml-language-server:latest",
-  root_dir = util.root_pattern(".git", vim.fn.getcwd()),
-}
+  cmd = lspcontainers.command("yamlls", {
+    image = "quay.io/redhat-developer/yaml-language-server:latest",
+  }),
+  -- root_markers = { ".git" },
+})
+
+vim.lsp.enable("gopls")
+vim.lsp.enable("jsonls")
+vim.lsp.enable("html")
+vim.lsp.enable("bashls")
+vim.lsp.enable("pylsp")
+vim.lsp.enable("terraformls")
+vim.lsp.enable("yamlls")
