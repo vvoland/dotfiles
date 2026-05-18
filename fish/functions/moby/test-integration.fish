f50aefe5b27b2b3dd8f641685db505330678fc66
Paweł Gronowski <git@grono.dev>
2026-05-18 14:59:26 +0200

    moby/test: fix graphdriver
    Signed-off-by: Paweł Gronowski <git@grono.dev>

--- fish/functions/moby/test-integration.fish
+++ fish/functions/moby/test-integration.fish
@@ -1,3 +1,3 @@
 function test-integration --arg filter
-	make BIND_DIR=. DOCKER_GRAPHDRIVER= TEST_IGNORE_CGROUP_CHECK=1 TEST_FILTER=$filter test-integration
+	make BIND_DIR=. TEST_INTEGRATION_USE_GRAPHDRIVER=1 DOCKER_GRAPHDRIVER=vfs TEST_IGNORE_CGROUP_CHECK=1 TEST_FILTER=$filter test-integration
 end
