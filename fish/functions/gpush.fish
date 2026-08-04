b9a329afcebd2799ed1a40bc405f218e7992970f
Paweł Gronowski <git@grono.dev>
2026-08-05 01:06:03 +0200

    fish/gpush: Push HEAD to a selected branch
    Accept a leading `-b <branch>` and turn it into the `HEAD:<branch>`
    refspec while preserving existing force-with-lease handling and other
    push options.
    
    Signed-off-by: Paweł Gronowski <git@grono.dev>

--- fish/functions/gpush.fish
+++ fish/functions/gpush.fish
@@ -1,4 +1,10 @@
 function gpush
+    set -l ref (git rev-parse --abbrev-ref HEAD)
+    if set -q argv[2]; and test "$argv[1]" = "-b"
+        set ref "HEAD:$argv[2]"
+        set -e argv[1..2]
+    end
+
     set -l args
     for arg in $argv
         if test "$arg" = "-f"; or test "$arg" = "--force"
@@ -7,5 +13,6 @@ function gpush
             set -a args $arg
         end
     end
-    git push origin (git rev-parse --abbrev-ref HEAD) $args
+
+    git push origin $ref $args
 end
