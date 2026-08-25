2a79f097d84f1ab669a496fdc97899bfca8c8085
Paweł Gronowski <git@grono.dev>
2026-08-26 01:11:27 +0200

    findPR: Open searches from SSH remotes
    Signed-off-by: Paweł Gronowski <git@grono.dev>

--- fish/functions/findPR.fish
+++ fish/functions/findPR.fish
@@ -1,5 +1,7 @@
 function findPR --arg ref
 	set upstreamUrl (git config --get remote.upstream.url)
+	set upstreamUrl (string replace -r '^git@github\.com:' 'https://github.com/' "$upstreamUrl")
+	set upstreamUrl (string replace -r '\.git$' '' "$upstreamUrl")
 	set url "$upstreamUrl/pulls?q=type%3Apr+$ref"
 	if test -f /usr/bin/open
 		open "$url"
