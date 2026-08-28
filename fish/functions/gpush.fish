c45f57c6e9b8401c7bff5ad0023ef044b14eb294
Paweł Gronowski <git@grono.dev>
2026-08-28 15:02:01 +0200

    fish/gpush: Push GitHub tree URLs to their branches
    Signed-off-by: Paweł Gronowski <git@grono.dev>

--- fish/functions/gpush.fish
+++ fish/functions/gpush.fish
@@ -1,8 +1,14 @@
 function gpush
+    set -l remote origin
     set -l ref (git rev-parse --abbrev-ref HEAD)
     if set -q argv[2]; and test "$argv[1]" = "-b"
         set ref "HEAD:$argv[2]"
         set -e argv[1..2]
+    else if set -q argv[1]; and string match -qr '^https://github\.com/[^/]+/[^/]+/tree/.+$' -- "$argv[1]"
+        set -l tree_url (string split -m 1 /tree/ -- "$argv[1]")
+        set remote $tree_url[1]
+        set ref "HEAD:$tree_url[2]"
+        set -e argv[1]
     end
 
     set -l args
@@ -14,5 +20,5 @@ function gpush
         end
     end
 
-    git push origin $ref $args
+    git push $remote $ref $args
 end
